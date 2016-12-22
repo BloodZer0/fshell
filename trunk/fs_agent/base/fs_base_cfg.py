78255500604873f0ed6353d6bbbee68942416fb6
Lingerhk <lingerhk@gmail.com>
2016-12-22 11:02:21 -0500

    update fs_base_cfg

--- trunk/fs_agent/base/fs_base_cfg.py
+++ trunk/fs_agent/base/fs_base_cfg.py
@@ -31,32 +31,105 @@ class AgentConf:
         self.conf.read(cfgFile)
 
 
-    def get_base_conf(self, option):
+##### agent base conf
+
+    def base_server_ip(self):
         try:
-            return self.conf.get("BASE", option)
+            return self.conf.get("BASE", "server_ip")
+        except:
+            return None
+    
+    def base_server_port(self):
+        try:
+            return self.conf.getint("BASE", "server_port")
+        except:
+            return None
+
+    def base_agent_id(self):
+        try:
+            return self.conf.get("BASE", "agent_id")
+        except:
+            return None
+        
+    def base_dev_name(self):
+        try:
+            return self.conf.get("BASE", "dev_name")
+        except:
+            return None
+        
+    def base_web_enum(self):
+        try:
+            return self.conf.get("BASE", "web_enum")
+        except:
+            return None
+
+    def base_web_dir(self):
+        try:
+            return self.conf.get("BASE", "web_dir")
+        except:
+            return None
+
+    def base_cache_dir(self):
+        try:
+            return self.conf.get("BASE", "cache_dir")
+        except:
+            return None
+
+    def base_log_dir(self):
+        try:
+            return self.conf.get("BASE", "log_dir")
+        except:
+            return None
+
+    def base_log_level(self):
+        try:
+            return self.conf.getint("BASE", "log_level")
+        except:
+            return 1
+
+    def base_log_prefix(self):
+        try:
+            return self.conf.get("BASE", "log_prefix")
+        except:
+            return ""
+
+    def base_ctr_log(self):
+        try:
+            return bool(self.conf.get("BASE", "ctr_log"))
+        except:
+            return True
+
+
+##### weblog conf
+
+    def weblog_log_file(self):
+        try:
+            return self.conf.get("WEBLOG", "log_file")
         except:
             return None
 
 
-    def get_weblog_conf(self, option):
-        try:
-            return self.conf.get("WEBLOG", option)
-        except:
-            return None
 
+"""
+##### statics conf
 
-    def get_statics_conf(option):
+    def statics_(self):
         try:
             return self.conf.get("STATICS", option)
         except:
             return None
 
 
+
+##### fileatt conf
+
     def get_fileatt_conf(option):
         try:
             return self.conf.get("FILEATT", option)
         except:
             return None
+
+##### danfunc conf
 
 
     def get_danfunc_conf(option):
@@ -65,6 +138,7 @@ class AgentConf:
         except:
             return None
 
+##### fuzzhash conf
 
     def get_fuzzhash_conf(option):
         try:
@@ -72,20 +146,30 @@ class AgentConf:
         except:
             return None
 
-
+"""
 
 class BaseConf:
 
     conf = AgentConf()
 
-    LOG_LEVEL       =   int(conf.get_base_conf("log_level"))
-    LOG_DIR         =   conf.get_base_conf("log_dir")
-    LOG_PREFIX      =   conf.get_base_conf("prefix")
-    IS_CTR_LOG      =   bool(conf.get_base_conf("is_ctr_log"))
+    AGENT_ID        =   conf.base_agent_id()
+    DEV_NAME        =   conf.base_dev_name()
+    WEB_ENUM        =   conf.base_web_enum()
+    WEB_DIR         =   conf.base_web_dir()
+    CACHE_DIR       =   conf.base_cache_dir()
 
-    SERVER_IP       =   conf.get_base_conf("server_ip")
-    SERVER_PORT     =   int(conf.get_base_conf("server_port"))
+    SERVER_IP       =   conf.base_server_ip()
+    SERVER_PORT     =   conf.base_server_port()
 
+    LOG_LEVEL       =   conf.base_log_level()
+    LOG_DIR         =   conf.base_log_dir()
+    LOG_PREFIX      =   conf.base_log_prefix()
+    CTR_LOG         =   conf.base_ctr_log()
+    
+    
+    LOG_FILE        =   conf.weblog_log_file()
+
+    
 
 
 if __name__ == "__main__":
