2f11cffc5ce2fe4463f010b2a8ed08ba35b0d0c1
Lingerhk <lingerhk@gmail.com>
2017-01-05 23:53:16 -0500

    update fss_srv_manager

--- trunk/fs_server/src/route/fss_srv_manager.py
+++ trunk/fs_server/src/route/fss_srv_manager.py
@@ -6,13 +6,11 @@
 # desc: srv manager
 
 
-
 from fss_cfg import *
 from fss_net import *
 from fss_srv_base import *
 from fss_proto_type import *
-
-#from fs_report_info_dao import *
+from fss_datastore import *
 
 
 class FsManagerSrv:
@@ -28,48 +26,15 @@ class FsManagerSrv:
         bRet = FsContentProto.check_valid(reqJson)
         if not bRet:  return False, ""
        
-
         Log.debug("recv_data: %s" % (reqJson))
 
-        # 记录心跳
-        """
-        HpsReportInfoDao.insert_node(
-            reqJson['task_id'], 
-            reqJson['dev_name'],
-            reqJson['plugin_id'],
-            reqJson['msg_protocol'],
-            reqJson['msg_type'],
-            reqJson['data'])
-        """
+        # data store
+        bRet, rspData = FsDataStore.store_data(reqJson)
+        if not bRet:
+            return False, ""
 
-        proto = reqJson['msg_type']
-        rspProto = FsProtoTypeEnum.F_DATA_WEBLOG
-        rspData = "from>>> fss_srv_manager.py"
+        rspEnum = rspData['proto_emum']
+        rspProto = rspData['proto_type']
+        rspData = rspData['data']
        
-        """
-        if proto == HpProtoTypeEnum.T_HPC_ROUTE_SYN_IP_REQ:
-            rspProto = HpProtoTypeEnum.T_HPC_ROUTE_SYN_IP_REQ
-            rspJson = {
-                'route_syn_ip' : ROUTE_SYN_IP,
-                'route_syn_port' : ROUTE_SYN_PORT
-            }
-            
-            rspData = json.dumps(rspJson)
-        
-        elif proto == HpProtoTypeEnum.T_HPC_ROUTE_ASY_IP_REQ:
-            rspProto = HpProtoTypeEnum.T_HPC_ROUTE_ASY_IP_REQ
-            rspJson = {
-                'route_asy_ip' : ROUTE_ASY_IP,
-                'route_asy_port' : ROUTE_ASY_PORT
-            }
-            
-            rspData = json.dumps(rspJson)
-            
-        else:
-            Log.err("proto: %d not valid!" %(proto))
-            rspProto = HpProtoTypeEnum.T_HP_ERR
-            rspData = ""   
-        """
-
-
-        return FsContentProto.response_packet(session, reqJson, FsProtoProtoEnum.F_RESULT_DOWN, rspProto, rspData)
+        return FsContentProto.response_packet(session, reqJson, rspEnum, rspProto, rspData)
