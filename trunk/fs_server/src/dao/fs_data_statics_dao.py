e2a8d5e9c4e87904017c0bdd4b82f9571459fa7e
Lingerhk <lingerhk@gmail.com>
2017-01-06 21:04:16 -0500

    add dao module

--- trunk/fs_server/src/dao/fs_data_statics_dao.py
+++ trunk/fs_server/src/dao/fs_data_statics_dao.py
@@ -4,6 +4,52 @@
 # author: s0nnet
 # time: 2017-01-06
 # desc: data_statics
+'''
+# 统计学-元数据表
+CREATE TABLE `tb_data_statics` (
+    `id` varchar(20) NOT NULL,
+    `agent_id` int(11) NOT NULL,
+    `filename` varchar(50) NOT NULL,
+    `text_ic` varchar(10) NOT NULL,
+    `text_ent` varchar(10) NOT NULL,
+    `text_lw` int(11) NOT NULL,
+    `text_cmp` varchar(10) NOT NULL,
+    `insert_tm` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+'''
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
+    sys.path.append("../base")
+
+
+from fs_cfg import *
+from fs_util import *
+from fs_database_pid import *
+
 
 class FsStaticsDao:
-    pass
+    
+    @staticmethod
+    def inert_node(agent_id, data):
+        filename = data['filename']
+        text_ic = data['text_ic']
+        text_ent = data['text_ent']
+        text_lw = data['text_lw']
+        text_cmp = text_cmp['text_cmp']
+
+        dataBase = DataBase()
+        sql = "insert into tb_data_statics(agent_id, filename, text_ic, text_ent, text_lw, \
+                text_cmp, insert_tm) values(%s, %s, %s, %s, %s, %s %s)"
+        param = (agent_id, filename, text_ic, text_ent, text_lw, text_lw, text_cmp, get_cur_time())
+
+        bRet, sRet = dataBase.insert_data(sql, param)
+        if not bRet:
+            return False, sRet
+
+        return True, sRet
+
+
+
--- trunk/fs_server/src/dao/fs_data_weblog_dao.py
+++ trunk/fs_server/src/dao/fs_data_weblog_dao.py
@@ -5,5 +5,54 @@
 # time: 2017-01-06
 # desc: data_weblog
 
+'''
+# web日志-元数据表
+CREATE TABLE `tb_data_weblog` (
+    `id` varchar(20) NOT NULL,
+    `agent_id` int(11) NOT NULL,
+    `client_ip` varchar(20) NOT NULL,
+    `time_local` datetime NOT NULL,
+    `method` varchar(6) NOT NULL,
+    `url` varchar(200) NOT NULL,
+    `req_body` text NOT NULL,
+    `referer` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+'''
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
+    sys.path.append("../base")
+
+
+from fs_cfg import *
+from fs_util import *
+from fs_database_pid import *
+
+
 class FsWebLogDao:
-    pass
+    
+    @staticmethod
+    def  insert_node(agent_id, data):
+        client_ip = data['client_ip']
+        time_local = data['time_local']
+        method = data['method']
+        url = data['url']
+        req_body = data['req_body']
+        referer = data['referer']
+
+
+        dataBase = DataBase()
+        sql = "insert into tb_data_weblog(agent_id, client_ip, time_local, method, url, \
+              req_body, referer) values(%s, %s, %s, %s, %s, %s, %s)";
+        param = (agent_id, client_ip, time_local, method, url, req_body, referer)
+        
+        bRet, sRet = DataBase.insert_data(sql, param)
+        if not bRet:
+            return False, sRet
+
+        return True, sRet
+
+
+
